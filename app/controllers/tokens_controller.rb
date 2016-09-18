9915813c2faa8509ceb7246ef08779b4628cab3b
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-18 23:35:01 +0900

    setting root

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,10 +1,10 @@
 class TokensController < ApplicationController
   def index
-    if params["hub.verify_token"] == "hogehoge"
+    if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
       render json: params["hub.challenge"]
     else
       render json: "Error, wrong validation token"
     end
   end
-  
+
 end
