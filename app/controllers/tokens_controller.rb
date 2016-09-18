8f7436d7556944af6aa44a99a4d67848f5f6626e
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-18 23:24:42 +0900

    setting callback

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,4 +1,10 @@
 class TokensController < ApplicationController
   def index
+    if params["hub.verify_token"] == "hogehoge"
+      render json: params["hub.challenge"]
+    else
+      render json: "Error, wrong validation token"
+    end
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'tokens/index'
-  root 'tokens#index'
+  root :to => 'tokens#index', via: "post"
 end
