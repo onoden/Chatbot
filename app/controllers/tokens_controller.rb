f257e9f680dd14dc7d26f32f95774a1505a51b43
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-19 00:09:24 +0900

    test bot response

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,9 +1,41 @@
 class TokensController < ApplicationController
+  # def index
+  #   if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
+  #     render json: params["hub.challenge"]
+  #   else
+  #     render json: "Error, wrong validation token"
+  #   end
+  # end
+
   def index
-    if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-      render json: params["hub.challenge"]
+    token = "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
+
+    message = params["entry"][0]["messaging"][0]
+
+    if message.include?("message")
+
+      #ユーザーの発言
+
+      sender = message["sender"]["id"]
+      text = message["message"]["text"]
+
+      endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
+      request_content = {recipient: {id:sender},
+                         message: {text: text}
+      }
+
+      content_json = request_content.to_json
+
+      RestClient.post(endpoint_uri, content_json, {
+          'Content-Type' => 'application/json; charset=UTF-8'
+      }){ |response, request, result, &block|
+        p response
+        p request
+        p result
+      }
     else
-      render json: "Error, wrong validation token"
+      #botの発言
+      "えらー"
     end
   end
 
