daec71807fa47f3f0abb704273a474ec4fadf38f
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-19 01:33:44 +0900

    test

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,7 +1,7 @@
 class TokensController < ApplicationController
   def index
     if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-      callback
+      # callback
       render json: params["hub.challenge"]
     else
       render json: "Error, wrong validation token"
