44c7b5a6a27f92bbf04de6c767af51c532c7dc07
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-19 00:35:25 +0900

    test bot response

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,13 +1,14 @@
 class TokensController < ApplicationController
-  # def index
-  #   if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-  #     render json: params["hub.challenge"]
-  #   else
-  #     render json: "Error, wrong validation token"
-  #   end
-  # end
-
   def index
+    if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
+      # render json: params["hub.challenge"]
+      callback
+    else
+      render json: "Error, wrong validation token"
+    end
+  end
+
+  def callback
     token = "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
 
     message = params["entry"][0]["messaging"][0]
@@ -34,8 +35,8 @@ class TokensController < ApplicationController
         p result
       }
     else
+      return 0
       #botの発言
-      "えらー"
     end
   end
 
