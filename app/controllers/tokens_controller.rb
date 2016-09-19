02de761fffb9aee4344a4c508fc24ff8533be18b
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-19 17:03:02 +0900

    test

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -2,8 +2,8 @@ class TokensController < ApplicationController
   protect_from_forgery with: :null_session, if: ->{request.format.json?}
   def index
     if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-      render json: params["hub.challenge"]
-      # callback
+      callback
+      # render json: params["hub.challenge"]
     else
       render json: "Error, wrong validation token"
     end
