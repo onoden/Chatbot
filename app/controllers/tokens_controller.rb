720f96e8e3865ddd7352cbfdf542396064f276fd
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-19 17:16:26 +0900

    callback test

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -10,6 +10,8 @@ class TokensController < ApplicationController
   end
 
   def callback
+    puts "callback起動"
+
     token = "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
 
     message = params["entry"][0]["messaging"][0]
