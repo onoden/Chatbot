f0a43608aaf5792fd608a9118b44d96964f461c1
Takashi Onoda <takashionoda@Takashi-no-MacBook-Pro-2.local>
2016-09-22 16:21:00 +0900

    fixed routing

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,44 +1,27 @@
 class TokensController < ApplicationController
   protect_from_forgery with: :null_session, if: ->{request.format.json?}
   def index
-    # if params["hub.verify_token"] == "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-    #   callback
-    #   "成功"
+    # if params["hub.verify_token"] == "hogehoge"
+    #   render json: params["hub.challenge"]
     # else
     #   render json: "Error, wrong validation token"
-    #   # render json: params["hub.challenge"]
-    #   puts params["hub.verify_token"]
-    #   p "えらー"
     # end
     callback
   end
 
   def callback
-    puts "callback起動"
-
     token = "EAAYq0g6YzFcBABj215kItqhn2XwjqZAjDcdEDXiCHivERslw3aHFGZBoYoDmbwpTeAxqn5onvTrCc6FI5k5bupvF8gcrmNqLHA9u7XrX5ZCZCkxToU9qfAzhAYonM8IyQ7LxqZBxIbpC9ZAcvDQiE53ONqWt7maVq0mX1YyCpYEAZDZD"
-
     unless params["entry"].nil?
       message = params["entry"][0]["messaging"][0]
-
-      p message
-
       if message.include?("message")
-
-        p "messageあった"
-
         #ユーザーの発言
-
         sender = message["sender"]["id"]
         text = message["message"]["text"]
-
         endpoint_uri = "https://graph.facebook.com/v2.6/me/messages?access_token=" + token
         request_content = {recipient: {id:sender},
                            message: {text: text}
         }
-
         content_json = request_content.to_json
-
         RestClient.post(endpoint_uri, content_json, {
             'Content-Type' => 'application/json; charset=UTF-8'
         }){ |response, request, result, &block|
@@ -46,13 +29,10 @@ class TokensController < ApplicationController
           p request
           p result
         }
-
-        p "post完了"
       else
         #botの発言
       end
     end
-
   end
 
 end
